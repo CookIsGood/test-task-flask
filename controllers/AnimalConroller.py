7347ddec267fa6e501b90f391529e1bf841c715a
CookIsGood <Morkovo4ka513@yandex.ru>
2022-02-03 17:45:26 +0300

    fix double post request

--- controllers/AnimalConroller.py
+++ controllers/AnimalConroller.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from services.AnimalService import AnimalService
 from forms import SearchForm, CreateAnimal
+
 
 animals = Blueprint('animals', __name__)
 
@@ -16,10 +17,17 @@ def search():
             'result_search': result_search,
         }
         return render_template("search.html", content=content, search_form=search_form, create_form=create_form)
-    if create_form.validate_on_submit():
+    return render_template("search.html", search_form=search_form, create_form=create_form, content=None)
+
+
+@animals.route('/create', methods=['GET', 'POST'])
+def create_animal():
+    search_form, create_form = SearchForm(), CreateAnimal()
+    if create_form.validate():
         animalService.create(create_form.name_animal.data,
                              create_form.type_animal.data,
                              create_form.speed_animal.data,
                              create_form.predator_animal.data)
         flash("Животное успешно создано!")
+        return redirect(url_for('animals.search'))
     return render_template("search.html", search_form=search_form, create_form=create_form, content=None)
